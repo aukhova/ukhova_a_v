8f625b5eb805c280f7cfc9aecbe1645c2dc94a67
aukhova <32308920+aukhova@users.noreply.github.com>
2017-10-08 21:25:01 +0300

    Add files via upload

---
+++ complex/complex.h
@@ -0,0 +1,52 @@
+#ifndef  COMPLEX_H
+#define COMPLEX_H
+#include <iosfwd>
+#include <iostream>
+#include <sstream>
+
+struct  Complex
+{
+	Complex() {}
+	explicit Complex(const double real);
+	Complex(const double real, const double imaginary);
+	bool operator ==(const Complex& rhs)const;
+	bool operator !=(const Complex& rhs)const;
+	Complex& operator+=(const Complex& rhs);
+	Complex& operator+=(const double rhs);
+	Complex& operator-=(const Complex& rhs);
+	Complex& operator-=(const double rhs);
+	Complex& operator*=(const Complex& rhs);
+	Complex& operator*=(const double rhs);
+	std::ostream& writeTo(std::ostream& ostrm)const;
+	std::istream& readFrom(std::istream& istrm);
+
+	double re{ 0.0 };
+	double im{ 0.0 };
+	double epsilon = 0.000000000001;
+
+	static const char leftBrace{ '{' };
+	static const char separator{ ',' };
+	static const char rightBrace{ '}' };
+};
+
+Complex operator+(const Complex& lhs, const Complex& rhs);
+Complex operator+(const Complex& lhs, const double rhs);
+Complex operator+(const double lhs, const Complex& rhs);
+
+Complex operator-(const Complex& lhs, const Complex& rhs);
+Complex operator-(const Complex& lhs, const double rhs);
+Complex operator-(const double lhs, const Complex& rhs);
+
+Complex operator*(const Complex& lhs, const Complex& rhs);
+Complex operator*(const Complex& lhs, const double rhs);
+Complex operator*(const double lhs, const Complex& rhs);
+
+
+inline std::ostream&operator<<(std::ostream& ostrm, const Complex& rhs);
+
+inline std::istream&operator >> (std::istream& istrm, Complex& rhs);
+
+bool testParse(const std::string& str);
+
+#endif // ! COMPLEX_H
+
